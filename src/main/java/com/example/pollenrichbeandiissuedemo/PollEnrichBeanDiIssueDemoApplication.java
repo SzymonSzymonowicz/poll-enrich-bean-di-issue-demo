ced690d779ab7f53030dacdb7e574cb306369e02
Szymon Szymonowicz <sz.szymonowicz5@gmail.com>
2025-07-28 11:56:14 +0200

    Demo project initialization

---
+++ src/main/java/com/example/pollenrichbeandiissuedemo/PollEnrichBeanDiIssueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pollenrichbeandiissuedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollEnrichBeanDiIssueDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollEnrichBeanDiIssueDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pollenrichbeandiissuedemo/PollEnrichBeanDiIssueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pollenrichbeandiissuedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollEnrichBeanDiIssueDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
